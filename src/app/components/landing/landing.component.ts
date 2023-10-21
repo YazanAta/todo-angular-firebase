e270961375b7b84c7badc48e45ded1ab857bd3a2
Yazan Ata <yazan.ata.01@outlook.com>
2023-10-22 01:00:01 +0300

    change font

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -2,16 +2,25 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription, fromEvent } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
 
   constructor(private router: Router, private as: AuthService){}
 
+  username : string
+
+  ngOnInit(): void {
+    this.as.user.subscribe((res)=> {
+      this.username = res?.displayName;
+    })
+  }
+  
 
   getStarted(){
 
